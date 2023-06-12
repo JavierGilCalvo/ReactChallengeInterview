1385d042a3fed0e12d11fdbdd392218fe29c7fd2
Javier Gil Calvo <jgil@isdefe.es>
2023-06-12 13:34:07 +0200

    Initial server commit

---
+++ hacker-news-coding-challenge/server/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = require('./src/routes/route.js')
+
+const app = expres()
+
+const PORT = process.env.PORT || 3001
+
+app.get('/', (req, res) => {
+    res.send('HOME')
+})
+
+app.use('/api/v1/', router)
+
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`)
+})
