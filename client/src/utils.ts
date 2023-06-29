1f751b99eefd5ff8d6ae8d911d599397e4bce904
13JaviG <13javiergc@gmail.com>
2023-06-29 21:18:22 +0200

    Date fix

--- client/src/utils.ts
+++ client/src/utils.ts
@@ -1,7 +1,7 @@
 export function getTimeOfItem (time: number) {
   const timeOfItem = new Date(time * 1000)
   const now = new Date()
-  const diff = Math.abs(now - timeOfItem)
+  const diff = Math.abs(now.getTime() - timeOfItem.getTime())
   const hour = diff / 36e5
   return Math.round(hour)
 }
